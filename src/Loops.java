464fc1ff0c04f174dd72a7829c8171f5219cdbee
Fahima Begum <haffahima@gmail.com>
2022-09-28 23:31:28 +0100

    first commit

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		//Loops allow performing action over and over again until something happens e.g. scoop 4 cups of flour into bowl for baking. Loops will scoop flour into cup until right amount met for cup.
+        //Types of loops: forloops(for)- if you have a specific no. of iterations you want to perform for something or have a group of items. 
+		//(variable iteration = x; boolean expression condition; post iteration/increment)
+		for (int i = 0; i < 10; i++) { //(variable iteration = x; boolean expression condition; post iteration/increment); 
+		}
+	}
+}
