efc28b6ed4b21f724d9dc12bb8d47040703dbc26
Fahima Begum <haffahima@gmail.com>
2022-10-01 16:20:13 +0100

    User input first commit

--- src/UserInput.java
+++ src/UserInput.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class UserInput {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); //object is scanner. Imported line 1 by right clicking scanner. Can now get input from the user
+		Scanner sc = new Scanner(System.in); //how to get user input from console. create object called Scanner, sc = identifier. Scanner is a data type like int, double,string etc. Imported line 1 by right clicking scanner. Can now get input from the user.
 		
+		System.out.println("Hello");
 		
 	}
 }
