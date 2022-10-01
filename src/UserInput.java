16822d3079fad5919f131d2bf939a284d4256e97
Fahima Begum <haffahima@gmail.com>
2022-10-01 18:44:09 +0100

    //note line 6th commit

--- src/UserInput.java
+++ src/UserInput.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class UserInput {
 	
 	public static void main(String[] args) {
-		try (Scanner sc = new Scanner(System.in)) { //java suggested instead of Scanner sc = Scanner(System.in) 
+		try (Scanner sc = new Scanner(System.in)) { //java suggested instead of Scanner sc = new Scanner(System.in); 
 			//how to get user input from console. create object called Scanner, sc = identifier. Scanner is a data type like int, double,string etc. Imported line 1 by right clicking scanner. Can now get input from the user.
 			
 			System.out.print("Enter your name: ");
